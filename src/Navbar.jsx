d7176a94a7601f1881bbfa00d40cd77aa67976ff
Ekak Basu <codebro125@gmail.com>
2026-04-04 18:14:46 +0530

    eat: TW-01 add homepage with Navbar, Hero and Footer components

---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+// src/Navbar.jsx
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <h2>TechCorp</h2>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Careers</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
